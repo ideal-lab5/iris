cca83d25cf8252da118571a985dd024d11e3a546
driemworks <tonyrriemer@gmail.com>
2022-07-13 13:16:13 -0500

    ipfs endpoint impls, add_args test

--- pallets/ipfs/src/ipfs.rs
+++ pallets/ipfs/src/ipfs.rs
@@ -49,28 +49,26 @@ pub struct IpfsAddRequest {
     bytes: Vec<u8>, 
 }
 
-/// Update the node's configuration
+/// Update the node's configuration. For the time being, we omit the optional
+/// bool and json arguments
 /// 
+/// http://127.0.0.1:5001/api/v0/config?arg=<key>&arg=<value>&bool=<value>&json=<value>
 /// 
 /// * config_item: The ipfs configuration to update. In general, this is a key-value pair.
 /// 
 pub fn config_update(config_item: IpfsConfigRequest) -> Result<(), http::Error> {
-    let endpoint = "http://127.0.0.1:5001/api/v0/config?";
-    // endpoint.push_str("arg=");
-    // match str::from_utf_8(config_item.key) {
-    //     Ok(k) => {
-    //         endpoint.push_str(k);
-    //     }, 
-    //     Err(_e) => {
-    //         return Err(http::Error::Unknown);
-    //     }
-    // }
-   
-    // "http://127.0.0.1:5001/api/v0/config?arg=<key>&arg=<value>&bool=<value>&json=<value>"
+    let mut endpoint = "http://127.0.0.1:5001/api/v0/config?".to_string();
+    endpoint = add_arg(endpoint, &"key".as_bytes().to_vec(), &config_item.key, true)
+        .map_err(|_| http::Error::Unknown).ok().unwrap();
+    endpoint = add_arg(endpoint, &"value".as_bytes().to_vec(), &config_item.value, false)
+        .map_err(|_| http::Error::Unknown).ok().unwrap();
+    ipfs_post_request(&endpoint, None)?;
     Ok(())
 }
 
 /// Show the node's current configuration
+/// 
+/// http://127.0.0.1:5001/api/v0/config/show
 /// 
 pub fn config_show() -> Result<http::Response, http::Error> {
     let endpoint = "http://127.0.0.1:5001/api/v0/config/show";
@@ -81,13 +79,16 @@ pub fn config_show() -> Result<http::Response, http::Error> {
 
 /// Connect to the given multiaddress
 /// 
+/// http://127.0.0.1:5001/api/v0/swarm/connect?arg={maddr}
+/// 
 /// * multiaddress: The multiaddress to connect to
 /// 
 pub fn connect(multiaddress: &Vec<u8>) -> Result<(), http::Error> {
     match str::from_utf8(multiaddress) {
         Ok(maddr) => {
-            let mut endpoint = "http://127.0.0.1:5001/api/v0/swarm/connect?arg=".to_owned();
-            endpoint.push_str(maddr);
+            let mut endpoint = "http://127.0.0.1:5001/api/v0/swarm/connect?";
+            endpoint = add_arg(endpoint, &"arg".as_bytes().to_vec(), &maddr, false)
+                .map_err(|_| http::Error::Unknown).ok().unwrap();
             ipfs_post_request(&endpoint, None).unwrap();
             return Ok(());
         },
@@ -99,13 +100,16 @@ pub fn connect(multiaddress: &Vec<u8>) -> Result<(), http::Error> {
 
 /// Disconeect from the given multiaddress
 /// 
+/// http://127.0.0.1:5001/api/v0/swarm/disconnect?arg={maddr}
+/// 
 /// * multiaddress: The multiaddress to disconnect from
 /// 
 pub fn disconnect(multiaddress: &Vec<u8>) -> Result<(), http::Error> {
     match str::from_utf8(multiaddress) {
         Ok(maddr) => {
-            let mut endpoint = "http://127.0.0.1:5001/api/v0/swarm/disconnect?arg=".to_owned();
-            endpoint.push_str(maddr);
+            let mut endpoint = "http://127.0.0.1:5001/api/v0/swarm/disconnect?";
+            endpoint = add_arg(endpoint, &"arg".as_bytes().to_vec(), &maddr, false)
+                .map_err(|_| http::Error::Unknown).ok().unwrap();
             ipfs_post_request(&endpoint, None).unwrap();
             return Ok(());
         },
@@ -117,6 +121,10 @@ pub fn disconnect(multiaddress: &Vec<u8>) -> Result<(), http::Error> {
 
 /// Add some data to ipfs
 /// 
+/// http://127.0.0.1:5001/api/v0/add
+/// 
+/// TODO: not yet implemented
+/// 
 /// * ipfs_add_request: The request object containing data to add
 /// 
 pub fn add(ipfs_add_request: IpfsAddRequest) -> Result<(), http::Error> {
@@ -126,13 +134,16 @@ pub fn add(ipfs_add_request: IpfsAddRequest) -> Result<(), http::Error> {
 
 /// Fetch data from the ipfs swarm and make it available from your node
 /// 
+/// http://127.0.0.1:5001/api/v0/get?
+/// 
 /// * cid: The CID to fetch
 /// 
 pub fn get(cid: &Vec<u8>) -> Result<(), http::Error> {
     match str::from_utf8(cid) {
         Ok(cid_string) => {
-            let mut endpoint = "http://127.0.0.1:5001/api/v0/get?arg=".to_owned();
-            endpoint.push_str(cid_string);
+            let mut endpoint = "http://127.0.0.1:5001/api/v0/get?";
+            endpoint = add_arg(endpoint, &"arg".as_bytes().to_vec(), &cid_string, false)
+                .map_err(|_| http::Error::Unknown).ok().unwrap();
             ipfs_post_request(&endpoint, None).unwrap();
             return Ok(());
         },
@@ -144,13 +155,16 @@ pub fn get(cid: &Vec<u8>) -> Result<(), http::Error> {
 
 /// retrieve data from IPFS and return it
 /// 
+/// http://127.0.0.1:5001/api/v0/cat?
+/// 
 /// cid: The CID to cat
 /// 
 pub fn cat(cid: &Vec<u8>) -> Result<http::Response, http::Error> {
     match str::from_utf8(cid) {
         Ok(cid_string) => {
-            let mut endpoint = "http://127.0.0.1:5001/api/v0/cat?arg=".to_owned();
-            endpoint.push_str(cid_string);
+            let mut endpoint = "http://127.0.0.1:5001/api/v0/cat?";
+            endpoint = add_arg(endpoint, &"arg".as_bytes().to_vec(), &cid_string, false)
+                .map_err(|_| http::Error::Unknown).ok().unwrap();
             let res = ipfs_post_request(&endpoint, None).ok();
             return Ok(res.unwrap());
         },
@@ -160,19 +174,36 @@ pub fn cat(cid: &Vec<u8>) -> Result<http::Response, http::Error> {
     }
 }
 
-// TODO: could replace http::Error with UTF8Error
-fn add_arg(endpoint: &str, arg: &Vec<u8>) -> Result<Vec<u8>, Utf8Error> {
+/// Append a key-value argument to the endpoint.
+/// Returns a Utf8Error if we fail to convert the key or value to utf8
+/// 
+/// e.g. (endpoint?, k, v, true) => endpoint?k=v&
+///      (endpoint?, k, v, false) => endpoint?k=v
+/// 
+fn add_arg(
+    endpoint: std::string::String,
+    key: &Vec<u8>,
+    value: &Vec<u8>,
+    append_and: bool,
+) -> Result<std::string::String, Utf8Error> {
     let mut endpoint = endpoint.to_owned();
-    endpoint.push_str("arg=");
-    match str::from_utf8(arg) {
-        Ok(argument) => {
-            endpoint.push_str(argument);
+    match str::from_utf8(key) {
+        Ok(k) => {
+            match str::from_utf8(value) {
+                Ok(v) => {
+                    endpoint.push_str(k);
+                    endpoint.push_str("=");
+                    endpoint.push_str(v);
+                    if append_and {
+                        endpoint.push_str("&");
+                    }
+                },
+                Err(e) => return Err(e),
+            }
         }, 
-        Err(e) => {
-            return Err(e);
-        }
+        Err(e) => return Err(e)
     }
-    Ok(endpoint.as_bytes().to_vec())
+    Ok(endpoint)
 }
 
 /// Make an http post request to IPFS
@@ -196,4 +227,28 @@ fn ipfs_post_request(endpoint: &str, body: Option<Vec<&[u8]>>) -> Result<http::R
         return Err(http::Error::Unknown);
     }
     Ok(response)
+}
+
+// #![cfg(test)]
+
+use super::*;
+use frame_support::{assert_noop, assert_ok, pallet_prelude::*};
+use sp_core::{
+	offchain::{testing, OffchainWorkerExt, TransactionPoolExt, OffchainDbExt}
+};
+
+
+#[test]
+pub fn ipfs_can_add_arg() {
+    let input = "https://localhost.com?".to_string();
+    let k_1 = "amphibian".as_bytes().to_vec();
+    let v_1 = "salamander".as_bytes().to_vec();
+
+    let k_2 = "reptile".as_bytes().to_vec();
+    let v_2 = "alligator".as_bytes().to_vec();
+
+    let expected_output = "https://localhost.com?amphibian=salamander&reptile=alligator";
+    let next_input = add_arg(input, &k_1, &v_1, true).unwrap();
+    let actual_output = add_arg(next_input, &k_2, &v_2, false).unwrap();
+    assert_eq!(expected_output, actual_output);
 }
