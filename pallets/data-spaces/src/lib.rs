65f078aa98e064034f95d0c8faed2bbfc6aa0c2b
driemworks <tonyrriemer@gmail.com>
2022-12-09 14:10:23 -0600

    remove unused crypto module

--- pallets/data-spaces/src/lib.rs
+++ pallets/data-spaces/src/lib.rs
@@ -72,36 +72,6 @@ pub struct DataSpaceMetadata<AssetId> {
     asset_ids: Vec<AssetId>,
 }
 
-pub mod crypto {
-	// use crate::KEY_TYPE;
-	use sp_core::crypto::KeyTypeId;
-	use sp_core::sr25519::Signature as Sr25519Signature;
-	use sp_runtime::app_crypto::{app_crypto, sr25519};
-	use sp_runtime::{traits::Verify, MultiSignature, MultiSigner};
-	use sp_std::convert::TryFrom;
-
-	pub const KEY_TYPE: KeyTypeId = KeyTypeId(*b"aura");
-
-	app_crypto!(sr25519, KEY_TYPE);
-
-	pub struct TestAuthId;
-	// implemented for runtime
-	impl frame_system::offchain::AppCrypto<MultiSigner, MultiSignature> for TestAuthId {
-		type RuntimeAppPublic = Public;
-		type GenericSignature = sp_core::sr25519::Signature;
-		type GenericPublic = sp_core::sr25519::Public;
-	}
-
-	// implemented for mock runtime in test
-	impl frame_system::offchain::AppCrypto<<Sr25519Signature as Verify>::Signer, Sr25519Signature>
-		for TestAuthId
-	{
-		type RuntimeAppPublic = Public;
-		type GenericSignature = sp_core::sr25519::Signature;
-		type GenericPublic = sp_core::sr25519::Public;
-	}
-}
-
 #[frame_support::pallet]
 pub mod pallet {
     use super::*;
